a3c12214cab8ee3609d89008f683e7a77ebaeb4b
Amir <118105282+amirgetalado@users.noreply.github.com>
2023-11-05 01:03:04 +0800

    Create function game()
    game() function loops the game making it playable for a few times
    until either the player or the computer reaches a score of 5
    then shows the final result

--- game.js
+++ game.js
@@ -15,38 +15,68 @@ function getComputerChoice(){
     }
 }
 
-
 //Create a function for 1 round of the game
-function playRound(playerSelection,computerSelection){
+function playRound(){
+
+    let playerSelection = prompt(`Please choose between rock, paper, and scissors: `); //store player choice to playerSelection
+        playerSelection = playerSelection.toLowerCase(); //make the choice of player lowercase
+
+    let computerSelection = getComputerChoice(); //store the computer choice to computerSelection
+
     //create conditions for winning/losing
     if ((playerSelection===`rock` && computerSelection===`scissors`) || 
     (playerSelection===`scissors` && computerSelection===`paper`) || 
     (playerSelection===`paper` && computerSelection===`rock`)){
         console.log(`You win! You chose "${playerSelection}" and the computer chose "${computerSelection}". ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`); //display player win
-        return winner = `player`;
+        return ++playerScore; //increment playerscore
     }else if ((playerSelection===`rock` && computerSelection===`rock`) || 
     (playerSelection===`paper` && computerSelection===`paper`) || 
     (playerSelection===`scissors` && computerSelection===`scissors`) ){
         console.log(`It's a draw. You chose "${playerSelection}" and the computer chose "${computerSelection}"`);//display draw
-        return winner = `draw`;
+        return;
     }else{
         console.log(`You lose! The computer chose "${computerSelection}" and you chose "${playerSelection}". ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}.`);//display computer win
-        return winner = `computer`;
+        return ++computerScore;
     }
 }
 
+//Create a function that plays the game 5 times
+ function game(){
+
+    //I use while loop because I set the max score to 5. First one reaches 5 will win
+    // while(playerScore<5 && computerScore <5){
+    //     playRound();
+    //     console.log(`Player Score: ${playerScore}`);
+    //     console.log(`Computer Score: ${computerScore}`);
+    // }
+
+    //The Odin Project's instruction is to play 5 rounds, this for loop is for looping until 5 rounds
+    for (let i =0; i<5; i++){
+        playRound();
+        console.log(`Player Score: ${playerScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+    }
+
+    if (playerScore > computerScore){
+        console.log(`------------------------------`);
+        console.log(`Congratulations. You are the winner.`);   
+        console.log(`Final score is: \nPlayer: ${playerScore} \nComputer: ${computerScore}`);
+    }else if(computerScore > playerScore) {
+        console.log(`------------------------------`);
+        console.log(`You lose. The computer wins.`);     
+        console.log(`Final score is: \nPlayer: ${playerScore} \nComputer: ${computerScore}`);
+    }else{
+        console.log(`------------------------------`);
+        console.log(`It's a tie.`);     
+        console.log(`Final score is: \nPlayer: ${playerScore} \nComputer: ${computerScore}`);
+    }  
+ }
 
 
-const computerSelection = getComputerChoice(); //store the computer choice to computerSelection
-
-let playerSelection = prompt(`Please choose between rock, paper, and scissors: `); //store player choice to playerSelection
-    playerSelection = playerSelection.toLowerCase(); //make the choice of player lowercase
 
 
 //for score storage
-let playerScore; 
-let computerScore;
-let winner = '';
+let playerScore=0; 
+let computerScore=0;
 
-playRound(playerSelection,computerSelection);
-// console.log(`The winner is ${winner}.`);
+game();
